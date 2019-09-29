5f90887acbf1e6f22b35185c703994bcede03509
takuya nakazawa <t.nakazawa0222@gmail.com>
2019-09-29 16:29:28 +0900

    微修正

--- tsuristagram/Modules/TabBar/TimeLine/TimeLineInteractor.swift
+++ tsuristagram/Modules/TabBar/TimeLine/TimeLineInteractor.swift
@@ -14,7 +14,7 @@ class TimeLineInteractor: TimeLineUsecase {
     // 取得処理の通知
     weak var delegate: TimeLineInteractorDelegate?
 
-    var limit:UInt = 5
+    var limit:UInt = 30
     // offsetの初期値は現在時間
     var offset: Int = (-1) * Int(Date().timeIntervalSince1970)
     var lastOffset: Int!
